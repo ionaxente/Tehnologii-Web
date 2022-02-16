c1f6fc9339838af85afc5172a3140ce90ec09944
ionaxente <ion.axente@gmail.com>
2022-02-16 16:45:51 +0200

    Edit app

--- ReactApp/src/App.js
+++ ReactApp/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Axente Ion RM-191
         </a>
       </header>
     </div>
